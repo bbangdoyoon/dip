f9f117880b4bafbd2964105388f970c090e2bb95
bbangdoyoon <hwangdo0924@naver.com>
2022-07-04 17:34:23 +0900

    220704

---
+++ python+work/220704/ex10.py
@@ -0,0 +1,17 @@
+# p.108
+# 다음은 빈 리스트를 만들어서 그 안에 1,2,3,을 넣었다가 넣은 순서대로 꺼내는 코드의 실행 흐름이다.
+# 위의 실행 흐름이 완성되도록 빈칸에 문장들을 채워 넣자
+st = []
+st.append(1)
+st.append(2)
+st.append(3)
+print(st)
+
+st.remove(1)  
+print(st)
+st.remove(2)
+print(st)
+st.remove(3)
+print(st)
+
+# shell에서 pop를 사용할 것
---
+++ python+work/220704/ex11.py
@@ -0,0 +1,18 @@
+#p109
+#다음 순서대로 값을 저장하고 꺼내도록 코드를 작성해보자
+#저장순서 1, 2, 3
+#꺼낸순서 3, 2, 1
+
+st = []
+st.append(1)
+st.append(2)
+st.append(3)
+print(st)
+
+st.pop(-1)
+print(st)
+st.pop(-1)
+print(st)
+st.pop(-1)
+print(st)
+
