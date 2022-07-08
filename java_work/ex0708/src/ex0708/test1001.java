5f91b4c0d441fed4d9c78a5e2379f99a4c2173c3
bbangdoyoon <hwangdo0924@naver.com>
2022-07-08 17:43:31 +0900

    220708

--- java_work/ex0708/src/ex0708/test1001.java
+++ java_work/ex0708/src/ex0708/test1001.java
@@ -12,11 +12,9 @@ public class test1001 {
 	public static void main(String[] args) {
 		Accumulator a = new Accumulator();
 		a.Accumulator();
-		for(int i = 0; i < 10; i++)
-			Accumulator.add(i);
-		Accumulator.showResult();
+		
 		
 	}
-	
-
 }
+
+
